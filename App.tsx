cff16b4801421ec4694746a7390883e10d140b8c
Juanjo <jjtorres.devtech@gmail.com>
2025-10-07 02:09:26 -0500

    add firebase SDK and build configuration

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>¡Bienvenido a PichangaApp! ⚽</Text>
+      <Text>¡Bienvenido a Pi</Text>
       <StatusBar style="auto" />
     </View>
   );
